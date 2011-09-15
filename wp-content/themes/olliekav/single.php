344b36fe0740b83d2aaf756ba1966af1c0dbf942
Ollie Kavanagh <ollie@kyanmedia.com>
2011-09-15 14:02:23 +0100

    Main setup files

---
+++ wp-content/themes/olliekav/single.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package WordPress
+ * @subpackage Toolbox
+ * @since Toolbox 0.1
+ */
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+				<?php toolbox_content_nav( 'nav-above' ); ?>
+
+				<?php get_template_part( 'content', 'single' ); ?>
+
+				<?php toolbox_content_nav( 'nav-below' ); ?>
+
+				<?php comments_template( '', true ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
