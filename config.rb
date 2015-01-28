228a0741ef46b9baabe1e65a341a6d097b87ca0d
Ollie Kavanagh <olliek@gmail.com>
2015-01-28 14:15:05 +0800

    Start new site design

--- config.rb
+++ config.rb
@@ -1,12 +1,8 @@
-set :markdown, :layout_engine => :haml
+Time.zone = "US/Eastern"
 
-### 
+###
 # Compass
 ###
-
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
 
 # Change Compass configuration
 # compass_config do |config|
@@ -14,102 +10,111 @@ set :markdown, :layout_engine => :haml
 # end
 
 ###
-# Haml
+# Page options, layouts, aliases and proxies
 ###
-
-# CodeRay syntax highlighting in Haml
-# First: gem install haml-coderay
-require 'haml-coderay'
-
-# CoffeeScript filters in Haml
-# First: gem install coffee-filter
-require 'coffee-filter'
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-activate :directory_indexes
-
-###
-# Page command
-###
-
-#active_nav = {:class => "active"}
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+page "/feed.xml", layout: false
+page "/404.html", :layout => false
+page "/sitemap.xml", :layout => false
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
 ###
 
-# Methods defined in the helpers block are available in templates
-helpers do
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
 
-  def conditional_html(options={}, &blk)
-    attrs = options.map { |(k, v)| " #{h k}=\"#{h v}\"" }.join('')
-    [ "<!--[if lt IE 9]>                 <html#{attrs} class=\"ie\"> <![endif]-->",
-      "<!--[if (gte IE 9)|!(IE)]><!-->   <html#{attrs}> <!--<![endif]-->",
-      capture_haml(&blk).strip,
-      "</html>"
-    ].join("\n")
-  end
-
-  def nav_active(page)
-    @page_id == page ? {:class => "active"} : {}
-  end
-
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload, :host => "thisiscapra.dev", :apply_js_live => true, :apply_css_live => true
+  config[:file_watcher_ignore] += [ /^build\// ]
 end
 
-# Change the CSS directory
-# set :css_dir, "alternative_css_directory"
+# Methods defined in the helpers block are available in templates
+helpers do
+  # Set the page title
+  def page_title
+    if content_for?(:title)
+      "#{yield_content(:title)} - Capra Design"
+    else
+      "Capra Design"
+    end
+  end
+  # Set the page description
+  def page_description
+    if content_for?(:description)
+      "#{yield_content(:description)}"
+    else
+      "Capra is a design agency based in Ottawa, Canada run by husband and wife team Ollie and Kat Kavanagh. Our focus is great design. We love interactive work like websites, games and apps because we get involved in building what we design."
+    end
+  end
+  # Active nav items
+  def nav_active(page)
+    current_page.url.start_with?(page) ? {:class => 'active'} : {}
+  end
+  # Custom page classes
+  def custom_page_classes
+    "page-#{page_classes} #{yield_content(:page_class) if content_for?(:page_class)}"
+  end
+  # Tag lists
+  def sentence_tag_list(article)
+    if tags = article.tags
+      content_tag(:div, class: :tags) do
+        "This article was filed under " +
+        article.tags.map{|t| link_to t, "/tags/#{t}"}.to_sentence +
+        "."
+      end
+    end
+  end
+  # Pretty dates
+  def pretty_date(date)
+    date.strftime('%B %d, %Y')
+  end
+  # Check if page
+  def page(path)
+    current_page.path == path
+  end
+end
 
-# Change the JS directory
-# set :js_dir, "alternative_js_directory"
+activate :directory_indexes
 
-# Change the images directory
-# set :images_dir, "alternative_image_directory"
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
-  
+
   # Minify Javascript on build
   activate :minify_javascript
-  
+
   # Enable cache buster
-  activate :cache_buster
-  
+  activate :asset_hash
+
   # Use relative URLs
   activate :relative_assets
-  
-  # activate :blog
 
-  #set :blog_permalink, "blog/:year/:title.html"
-  #set :blog_layout_engine, "haml"
-  #set :blog_taglink, "categories/:tag.html"
-  
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-  
   # Or use a different image path
-  # set :http_path, "/Content/images/"
+  # set :http_prefix, "/Content/images/"
 end
