846d7857ba3e23719afee3eb7477e26eae0a145e
Ollie Kavanagh <olliek@gmail.com>
2015-02-03 11:54:08 +0800

    Fixes for ie9

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -21,12 +21,13 @@ var removeClass = function (elem, className) {
 if ( 'querySelector' in document && 'addEventListener' in window ) {
 
   var body = document.querySelector('body'),
+      html = document.querySelector('html'),
+      height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
       width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
       menuLink = document.getElementById('resp-nav'),
       top = document.documentElement.scrollTop || document.body.scrollTop;
 
-  console.log(menuLink)
-
+  // Add scrolling class to body
   var fired = false;
   window.addEventListener('scroll', function() {
     if(window.pageYOffset >= 100 && fired === false) {
@@ -40,6 +41,7 @@ if ( 'querySelector' in document && 'addEventListener' in window ) {
     }
   });
 
+  // Responsive nav menu
   menuLink.addEventListener("click", function(event){ 
     if (hasClass(body, 'resp')) {
       removeClass(body, 'resp')
@@ -51,5 +53,17 @@ if ( 'querySelector' in document && 'addEventListener' in window ) {
     }
     event.preventDefault();
   });
+
+  // ie9 fixes for flexbox
+  if(hasClass(html, 'ie9')) {
+    function reSize() {
+      [].forEach.call(document.querySelectorAll('.client'), function(el) {
+        var itemHeight = body.offsetHeight / 3;
+        el.style.height = itemHeight + 'px';
+      })
+    }
+    reSize();
+    window.addEventListener('resize', reSize, false);
+  }
 
 }
