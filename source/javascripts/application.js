38383e145a35e4ebd261d95e5d841cf32bc5fbe8
Ollie Kavanagh <olliek@gmail.com>
2015-02-15 17:10:25 +0800

    Try and fix the JS again

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -50,7 +50,6 @@ if ( 'querySelector' in document && 'addEventListener' in window ) {
   function responsiveNav() {
     var menuLink = document.querySelector('.resp-nav');
     menuLink.addEventListener("click", function(event){ 
-      console.log('clicked');
       if (hasClass(html, 'resp')) {
         removeClass(html, 'resp')
         removeClass(menuLink, 'open')
@@ -86,89 +85,11 @@ if ( 'querySelector' in document && 'addEventListener' in window ) {
     }
   }
 
-  // Music player
-  function tonedenInit() {
-    ToneDenReady = window.ToneDenReady || [];
-    var configOne = {
-      dom: "#player-1",
-      urls: [
-        "https://soundcloud.com/olliekav/ollie-k-dnb-mix-270513"
-      ]
-    }
-    var configTwo = {
-      dom: "#player-2",
-      urls: [
-        "https://soundcloud.com/olliekav/ollie-k-drum-bass-mix-2000"
-      ]
-    }
-    var configThree = {
-      dom: "#player-3",
-      urls: [
-        "https://soundcloud.com/olliekav/drum-and-bass-mix-2005"
-      ]
-    }
-    var configFour = {
-      dom: "#player-4",
-      urls: [
-        "https://soundcloud.com/olliekav/olliek-mixjune04"
-      ]
-    }
-    var configFive = {
-      dom: "#player-5",
-      urls: [
-        "https://soundcloud.com/olliekav/ollie-k-house-techno-mix-21-02"
-      ]
-    }
-    var configSix = {
-      dom: "#player-6",
-      urls: [
-        "https://soundcloud.com/olliekav/ollie-k-house-techno-mix-19-05"
-      ]
-    }
-    var configSeven = {
-      dom: "#player-7",
-      urls: [
-        "https://soundcloud.com/olliekav/house-techno-mix-12-12-08"
-      ]
-    }
-
-    // ToneDenReady.push(function() {
-    //   ToneDen.player.create(configOne);
-    //   ToneDen.player.create(configTwo);
-    //   ToneDen.player.create(configThree);
-    //   ToneDen.player.create(configFour);
-    //   ToneDen.player.create(configFive);
-    //   ToneDen.player.create(configSix);
-    //   ToneDen.player.create(configSeven);
-    // }); 
-
-    if(typeof ToneDen != 'undefined'){
-      ToneDen.player.create(configOne);
-      ToneDen.player.create(configTwo);
-      ToneDen.player.create(configThree);
-      ToneDen.player.create(configFour);
-      ToneDen.player.create(configFive);
-      ToneDen.player.create(configSix);
-      ToneDen.player.create(configSeven);
-    } else {
-      ToneDenReady.push(function() {
-        ToneDen.player.create(configOne);
-        ToneDen.player.create(configTwo);
-        ToneDen.player.create(configThree);
-        ToneDen.player.create(configFour);
-        ToneDen.player.create(configFive);
-        ToneDen.player.create(configSix);
-        ToneDen.player.create(configSeven);
-      }); 
-    }
-  }
-
   document.addEventListener("DOMContentLoaded", function() {
     scrolling();
     responsiveNav();
     fixie9();
     svglink();
-    tonedenInit();
   });
 
   document.addEventListener("page:fetch", function() {
