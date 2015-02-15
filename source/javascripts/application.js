866e69293a044e3b3b49bc2fc414dd35b1970726
Ollie Kavanagh <olliek@gmail.com>
2015-02-15 16:30:23 +0800

    Fix the music page again

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -86,11 +86,90 @@ if ( 'querySelector' in document && 'addEventListener' in window ) {
     }
   }
 
+  // Music player
+  function tonedenInit() {
+    console.log("I'm running")
+    ToneDenReady = window.ToneDenReady || [];
+    var configOne = {
+      dom: "#player-1",
+      urls: [
+        "https://soundcloud.com/olliekav/ollie-k-dnb-mix-270513"
+      ]
+    }
+    var configTwo = {
+      dom: "#player-2",
+      urls: [
+        "https://soundcloud.com/olliekav/ollie-k-drum-bass-mix-2000"
+      ]
+    }
+    var configThree = {
+      dom: "#player-3",
+      urls: [
+        "https://soundcloud.com/olliekav/drum-and-bass-mix-2005"
+      ]
+    }
+    var configFour = {
+      dom: "#player-4",
+      urls: [
+        "https://soundcloud.com/olliekav/olliek-mixjune04"
+      ]
+    }
+    var configFive = {
+      dom: "#player-5",
+      urls: [
+        "https://soundcloud.com/olliekav/ollie-k-house-techno-mix-21-02"
+      ]
+    }
+    var configSix = {
+      dom: "#player-6",
+      urls: [
+        "https://soundcloud.com/olliekav/ollie-k-house-techno-mix-19-05"
+      ]
+    }
+    var configSeven = {
+      dom: "#player-7",
+      urls: [
+        "https://soundcloud.com/olliekav/house-techno-mix-12-12-08"
+      ]
+    }
+
+    // ToneDenReady.push(function() {
+    //   ToneDen.player.create(configOne);
+    //   ToneDen.player.create(configTwo);
+    //   ToneDen.player.create(configThree);
+    //   ToneDen.player.create(configFour);
+    //   ToneDen.player.create(configFive);
+    //   ToneDen.player.create(configSix);
+    //   ToneDen.player.create(configSeven);
+    // }); 
+
+    if(typeof ToneDen != 'undefined'){
+      ToneDen.player.create(configOne);
+      ToneDen.player.create(configTwo);
+      ToneDen.player.create(configThree);
+      ToneDen.player.create(configFour);
+      ToneDen.player.create(configFive);
+      ToneDen.player.create(configSix);
+      ToneDen.player.create(configSeven);
+    } else {
+      ToneDenReady.push(function() {
+        ToneDen.player.create(configOne);
+        ToneDen.player.create(configTwo);
+        ToneDen.player.create(configThree);
+        ToneDen.player.create(configFour);
+        ToneDen.player.create(configFive);
+        ToneDen.player.create(configSix);
+        ToneDen.player.create(configSeven);
+      }); 
+    }
+  }
+
   document.addEventListener("DOMContentLoaded", function() {
     scrolling();
     responsiveNav();
     fixie9();
     svglink();
+    tonedenInit();
   });
 
   document.addEventListener("page:fetch", function() {
@@ -100,15 +179,14 @@ if ( 'querySelector' in document && 'addEventListener' in window ) {
     scrolling();
     responsiveNav();
     svglink();
+    fixie9();
     window.scrollTo(0,0);
   });
 
   document.addEventListener("page:load", function() {
     removeClass(html, "animate-out");
     addClass(html, "animate-in");
-    responsiveNav();
-    fixie9();
-    svglink();
+    tonedenInit();
   });
 
 }
---
+++ source/javascripts/toneden.js
@@ -0,0 +1,63 @@
+ToneDenReady = window.ToneDenReady || [];
+var configOne = {
+  dom: "#player-1",
+  urls: [
+    "https://soundcloud.com/olliekav/ollie-k-dnb-mix-270513"
+  ]
+}
+var configTwo = {
+  dom: "#player-2",
+  urls: [
+    "https://soundcloud.com/olliekav/ollie-k-drum-bass-mix-2000"
+  ]
+}
+var configThree = {
+  dom: "#player-3",
+  urls: [
+    "https://soundcloud.com/olliekav/drum-and-bass-mix-2005"
+  ]
+}
+var configFour = {
+  dom: "#player-4",
+  urls: [
+    "https://soundcloud.com/olliekav/olliek-mixjune04"
+  ]
+}
+var configFive = {
+  dom: "#player-5",
+  urls: [
+    "https://soundcloud.com/olliekav/ollie-k-house-techno-mix-21-02"
+  ]
+}
+var configSix = {
+  dom: "#player-6",
+  urls: [
+    "https://soundcloud.com/olliekav/ollie-k-house-techno-mix-19-05"
+  ]
+}
+var configSeven = {
+  dom: "#player-7",
+  urls: [
+    "https://soundcloud.com/olliekav/house-techno-mix-12-12-08"
+  ]
+}
+
+if(typeof ToneDen != 'undefined'){
+  ToneDen.player.create(configOne);
+  ToneDen.player.create(configTwo);
+  ToneDen.player.create(configThree);
+  ToneDen.player.create(configFour);
+  ToneDen.player.create(configFive);
+  ToneDen.player.create(configSix);
+  ToneDen.player.create(configSeven);
+} else {
+  ToneDenReady.push(function() {
+    ToneDen.player.create(configOne);
+    ToneDen.player.create(configTwo);
+    ToneDen.player.create(configThree);
+    ToneDen.player.create(configFour);
+    ToneDen.player.create(configFive);
+    ToneDen.player.create(configSix);
+    ToneDen.player.create(configSeven);
+  }); 
+}
