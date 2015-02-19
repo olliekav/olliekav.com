eb2e5a520541422b2a5374c4e9cc16f17ed1ab0f
Ollie Kavanagh <olliek@gmail.com>
2015-02-18 20:00:17 -0500

    Restructure the JS again to be more compact

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -65,10 +65,99 @@ if ( 'querySelector' in document && 'addEventListener' in window ) {
       window.addEventListener('resize', reSize, false);
     }
 
+    tonedenInit = function () {
+      if(hasClass(wrapper, 'page-music')) {
+        // First append the script, but only if it doesn't exist already
+        var scripts = document.getElementsByTagName("script");
+        var headerAdded = false;
+        for (var i=0; i< scripts.length; i++){
+          if (scripts[i].src == "//sd.toneden.io/production/toneden.loader.js"){
+            headerAdded = true;
+          }
+        }
+        if (headerAdded == false){
+          (function() {
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            script.async = true;
+            script.src = "//sd.toneden.io/production/toneden.loader.js"
+            var entry = document.getElementsByTagName("script")[0];
+            entry.parentNode.insertBefore(script, entry);
+          }());
+        }
+
+        ToneDenReady = window.ToneDenReady || [];
+        var configOne = {
+          dom: "#player-1",
+          urls: [
+            "https://soundcloud.com/olliekav/ollie-k-dnb-mix-270513"
+          ]
+        }
+        var configTwo = {
+          dom: "#player-2",
+          urls: [
+            "https://soundcloud.com/olliekav/ollie-k-drum-bass-mix-2000"
+          ]
+        }
+        var configThree = {
+          dom: "#player-3",
+          urls: [
+            "https://soundcloud.com/olliekav/drum-and-bass-mix-2005"
+          ]
+        }
+        var configFour = {
+          dom: "#player-4",
+          urls: [
+            "https://soundcloud.com/olliekav/olliek-mixjune04"
+          ]
+        }
+        var configFive = {
+          dom: "#player-5",
+          urls: [
+            "https://soundcloud.com/olliekav/ollie-k-house-techno-mix-21-02"
+          ]
+        }
+        var configSix = {
+          dom: "#player-6",
+          urls: [
+            "https://soundcloud.com/olliekav/ollie-k-house-techno-mix-19-05"
+          ]
+        }
+        var configSeven = {
+          dom: "#player-7",
+          urls: [
+            "https://soundcloud.com/olliekav/house-techno-mix-12-12-08"
+          ]
+        }
+        if(typeof ToneDen != 'undefined'){
+          console.log('undefined')
+          ToneDen.player.create(configOne);
+          ToneDen.player.create(configTwo);
+          ToneDen.player.create(configThree);
+          ToneDen.player.create(configFour);
+          ToneDen.player.create(configFive);
+          ToneDen.player.create(configSix);
+          ToneDen.player.create(configSeven);
+        } else {
+          ToneDenReady.push(function() {
+            console.log('ToneDen')
+            ToneDen.player.create(configOne);
+            ToneDen.player.create(configTwo);
+            ToneDen.player.create(configThree);
+            ToneDen.player.create(configFour);
+            ToneDen.player.create(configFive);
+            ToneDen.player.create(configSix);
+            ToneDen.player.create(configSeven);
+          });
+        }
+      }
+    }
+
     if(hasClass(html, 'resp')) {
       removeClass(html, 'resp');
     }
 
+    tonedenInit();
     workNav();
 
     if(hasClass(html, 'ie9')) {
@@ -77,76 +166,8 @@ if ( 'querySelector' in document && 'addEventListener' in window ) {
 
   }; 
 
-  tonedenInit = function () {
-    var configOne = {
-      dom: "#player-1",
-      urls: [
-        "https://soundcloud.com/olliekav/ollie-k-dnb-mix-270513"
-      ]
-    }
-    var configTwo = {
-      dom: "#player-2",
-      urls: [
-        "https://soundcloud.com/olliekav/ollie-k-drum-bass-mix-2000"
-      ]
-    }
-    var configThree = {
-      dom: "#player-3",
-      urls: [
-        "https://soundcloud.com/olliekav/drum-and-bass-mix-2005"
-      ]
-    }
-    var configFour = {
-      dom: "#player-4",
-      urls: [
-        "https://soundcloud.com/olliekav/olliek-mixjune04"
-      ]
-    }
-    var configFive = {
-      dom: "#player-5",
-      urls: [
-        "https://soundcloud.com/olliekav/ollie-k-house-techno-mix-21-02"
-      ]
-    }
-    var configSix = {
-      dom: "#player-6",
-      urls: [
-        "https://soundcloud.com/olliekav/ollie-k-house-techno-mix-19-05"
-      ]
-    }
-    var configSeven = {
-      dom: "#player-7",
-      urls: [
-        "https://soundcloud.com/olliekav/house-techno-mix-12-12-08"
-      ]
-    }
-    if(hasClass(wrapper, 'page-music')) {
-      if(typeof ToneDen != 'undefined'){
-        console.log('undefined')
-        ToneDen.player.create(configOne);
-        ToneDen.player.create(configTwo);
-        ToneDen.player.create(configThree);
-        ToneDen.player.create(configFour);
-        ToneDen.player.create(configFive);
-        ToneDen.player.create(configSix);
-        ToneDen.player.create(configSeven);
-      } else {
-        ToneDenReady.push(function() {
-          ToneDen.player.create(configOne);
-          ToneDen.player.create(configTwo);
-          ToneDen.player.create(configThree);
-          ToneDen.player.create(configFour);
-          ToneDen.player.create(configFive);
-          ToneDen.player.create(configSix);
-          ToneDen.player.create(configSeven);
-        });
-      }
-    }
-  }
-
   document.addEventListener("DOMContentLoaded", function() {
     ready();
-    tonedenInit();
     window.addEventListener("click", respNav, false);
   }, false);
 
@@ -157,7 +178,6 @@ if ( 'querySelector' in document && 'addEventListener' in window ) {
 
   document.addEventListener("page:load", function() {
     ready();
-    tonedenInit();
     window.scrollTo(0,0);
   }, false);
 
